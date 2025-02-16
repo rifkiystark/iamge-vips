489fb7b1d3f41d658e5ed2250562442256757ff5
anandarifkiy.rea <ananda.rifkiy@ruangguru.com>
2025-02-16 11:35:09 +0700

    get port from env

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use libvips::VipsApp;
+use std::env;
 
 mod handlers;
 mod routes;
@@ -9,7 +10,9 @@ async fn main() {
     let app = VipsApp::new("VipsApp", true).expect("Failed to initialize VipsApp");
     app.concurrency_set(2);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, routes::create_routes())
         .await
         .unwrap();
